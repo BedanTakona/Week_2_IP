2cf869980a314476808608b97aee83b5ea25292d
BedanTakona <bedanmaison35@gmail.com>
2021-02-22 15:44:22 +0300

    Added DOM method to script sheet

---
+++ script.js
@@ -0,0 +1,49 @@
+document.getElementById('subbtn').addEventListener('click', function getName(e){
+    e.preventDefault();
+    var date = document.getElementById('date').value;
+    var gender = document.getElementById('gender').value;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
